9afbc14a96f313fc19a5d8f63a747d72c363f049
madwoman <jkosolap@gmail.com>
2022-12-28 17:05:55 +0200

    Initial commit

---
+++ src/main/java/MyApp.java
@@ -0,0 +1,11 @@
+import com.google.gson.Gson;
+
+import java.util.Map;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+        Map<String, String> myName = Map.of("name", "Julia", "lastName", "Kosolap");
+        System.out.println(new Gson().toJson(myName));
+    }
+}
